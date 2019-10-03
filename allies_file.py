7c7a8db9b60d5fd47a190decb5e9e90b9792085f
Zac Mazza <zac.mazza85@gmail.com>
2019-10-02 21:07:01 -0500

    Added initial framework

--- allies_file.py
+++ allies_file.py
@@ -1,1 +1,23 @@
 print("Hello!")
+
+## Response variable holds response from human
+response = input("How are you?: >>> ")
+if(response.lower() == "good"):
+    print("Great!")
+else:
+    print("Thats a bummer")
+
+print("Check out my choose your own adventure game!")
+response = input("Which direction do you want to go?: >>> ")
+if(response == "up"):
+    print("Going up...")
+elif(response == "down"):
+    print("Going down...")
+elif(response == "left"):
+    print("Going left...")
+elif(response == "right"):
+    print("Going right...")
+else:
+    print("Are you sure you really know where you are going?")
+
+print("Continuing the story...")
